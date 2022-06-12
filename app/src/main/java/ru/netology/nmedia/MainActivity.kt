a1e0026389ded7479b361baf493a8e0f18122e00
Сергей Акимов <akimovserega@gmail.com>
2022-06-12 15:17:54 +0500

    the second activity - post

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,11 +1,20 @@
 package ru.netology.nmedia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+    fun openPostList(view: View) {
+        val intent = Intent(this, PostList::class.java)
+        startActivity(intent)
+    }
+    
 }
