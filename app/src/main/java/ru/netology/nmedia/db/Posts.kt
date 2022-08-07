06194be8501ede0fcaa045038ec76c1db05c840e
Сергей Акимов <akimovserega@gmail.com>
2022-08-07 14:40:59 +0500

    room migration

--- app/src/main/java/ru/netology/nmedia/db/Posts.kt
+++ app/src/main/java/ru/netology/nmedia/db/Posts.kt
@@ -3,16 +3,30 @@ package ru.netology.nmedia.db
 import android.database.Cursor
 import ru.netology.nmedia.data.Post
 
-fun Cursor.toPost() = Post (
+internal fun PostEntity.toModel() = Post(
 
-    id = getLong(getColumnIndexOrThrow(PostsTable.Column.ID.columnName)),
-    author =   getString(getColumnIndexOrThrow(PostsTable.Column.AUTHOR.columnName)),
-    content =   getString(getColumnIndexOrThrow(PostsTable.Column.CONTENT.columnName)),
-    published =   getString(getColumnIndexOrThrow(PostsTable.Column.PUBLISHED.columnName)),
-    likedByMe = getInt(getColumnIndexOrThrow(PostsTable.Column.LIKED_BY_ME.columnName))!=0,
-    likes =   getInt(getColumnIndexOrThrow(PostsTable.Column.LIKES.columnName)),
-    shares =   getInt(getColumnIndexOrThrow(PostsTable.Column.SHARES.columnName)),
-    views =   getInt(getColumnIndexOrThrow(PostsTable.Column.VIEWS.columnName)),
-    video =   getString(getColumnIndexOrThrow(PostsTable.Column.VIDEO.columnName))
+    id = id,
+    author = author,
+    content = content,
+    published = published,
+    likedByMe = likedByMe,
+    likes = likes,
+    shares = shares,
+    views = views,
+    video = video
+
+)
+
+internal fun Post.toEntity() = PostEntity(
+
+    id = id,
+    author = author,
+    content = content,
+    published = published,
+    likedByMe = likedByMe,
+    likes = likes,
+    shares = shares,
+    views = views,
+    video = video
 
 )
